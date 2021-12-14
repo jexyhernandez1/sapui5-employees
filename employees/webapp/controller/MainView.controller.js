d526b3cc76beab8a64263dac3ba52ff8dca77def
jexyhernandez1 <jexyhernandez.1@gmail.com>
2021-12-14 00:16:43 +0000

    Firts commit Elemento_Contr_UI5

--- employees/webapp/controller/MainView.controller.js
+++ employees/webapp/controller/MainView.controller.js
@@ -7,9 +7,23 @@ sap.ui.define([
     function (Controller) {
         "use strict";
 
-        return Controller.extend("logaligroup.employees.controller.MainView", {
-            onInit: function () {
+        var Main = Controller.extend("logaligroup.employees.controller.MainView",{});
 
-            }
+        Main.prototype.onValidate = function(){
+
+                var inputEmployee = this.byId("inputEmployee");
+                var valueEmployee = inputEmployee.getValue();
+    
+                if(valueEmployee.length === 6 ){    
+//                  inputEmployee.setDescription("OK");
+                    this.byId("labelCountry").setVisible(true);  
+                    this.byId("slCountry").setVisible(true);  
+               }else{
+//                   inputEmployee.setDescription("Not OK");
+                     this.byId("labelCountry").setVisible(false);  
+                     this.byId("slCountry").setVisible(false);
+               }   
+    
+            };
+            return Main;   
         });
-    });
